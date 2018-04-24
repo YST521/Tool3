6d82c2cfde7d56f5a6be3342eaad8085fb8c6553
youxin <>
2018-04-24 16:32:21 +0800

    增加一个封装播放器 可实现 cell上播放 悬停 仿抖音上滑切换的效果

---
+++ cell上的播放器----JPVideoPlayer/JPVideoPlayerDemo/JPVideoPlayerDemo/AppDelegate.m
@@ -0,0 +1,60 @@
+/*
+ * This file is part of the JPVideoPlayer package.
+ * (c) NewPan <13246884282@163.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Click https://github.com/newyjp
+ * or http://www.jianshu.com/users/e2f2d779c022/latest_articles to contact me.
+ */
+
+#import "AppDelegate.h"
+#import "JPVideoPlayerWeiBoViewController.h"
+#import "JPVideoPlayerSettingViewController.h"
+#import "JPVideoPlayerDouyinViewController.h"
+#import "JPVideoPlayerManager.h"
+#import <JPNavigationControllerKit.h>
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // 启动图片延时: 2 秒
+//    [NSThread sleepForTimeInterval:2];
+    
+ //   https://juejin.im/post/5ad168c2518825555e5e44dd
+    
+    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
+    
+    JPNavigationController *weiboNavigationController = [[JPNavigationController alloc]initWithRootViewController:[JPVideoPlayerWeiBoViewController new]];
+    weiboNavigationController.tabBarItem.image = [[UIImage imageNamed:@"jp_videoplayer_weibo"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    weiboNavigationController.tabBarItem.selectedImage = [[UIImage imageNamed:@"jp_videoplayer_weibo_selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    weiboNavigationController.title = @"微博";
+
+    JPVideoPlayerDouyinViewController *douyinViewController = [JPVideoPlayerDouyinViewController new];
+    douyinViewController.tabBarItem.image = [[UIImage imageNamed:@"jp_videoplayer_douyin"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    douyinViewController.tabBarItem.selectedImage = [[UIImage imageNamed:@"jp_videoplayer_douyin_selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    douyinViewController.title = @"抖音";
+    
+    JPNavigationController *settingNavigationController = [[UINavigationController alloc]initWithRootViewController:[JPVideoPlayerSettingViewController new]];
+    settingNavigationController.tabBarItem.image = [[UIImage imageNamed:@"jp_videoplayer_setting"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    settingNavigationController.tabBarItem.selectedImage = [[UIImage imageNamed:@"jp_videoplayer_setting_selected"] imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    settingNavigationController.title = @"设置";
+    
+    UITabBarController *tabVC = [[UITabBarController alloc]init];
+    tabVC.viewControllers = @[weiboNavigationController, douyinViewController,settingNavigationController];
+    tabVC.tabBar.tintColor = [UIColor blackColor];
+    tabVC.tabBar.backgroundImage = [UIImage imageNamed:@"jp_videoplayer_tabbar"];
+    
+    self.window.rootViewController = tabVC;
+    [self.window makeKeyAndVisible];
+    [[UIApplication sharedApplication]setStatusBarHidden:NO withAnimation:UIStatusBarAnimationFade];
+
+    return YES;
+}
+
+@end
